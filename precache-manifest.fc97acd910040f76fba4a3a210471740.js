6cdc692b36baaaaff65865f408453b1a373d329d
naderalfakesh <naderalfakesh@gmail.com>
2019-12-14 22:57:35 +0300

    Updates

---
+++ precache-manifest.fc97acd910040f76fba4a3a210471740.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44cf01e8df3a5a5ebd8f819b8838c58f",
+    "url": "/coin-flipper/index.html"
+  },
+  {
+    "revision": "271ee74172d39c0b232d",
+    "url": "/coin-flipper/static/css/main.d7d75902.chunk.css"
+  },
+  {
+    "revision": "4f9f91fff8131483efcf",
+    "url": "/coin-flipper/static/js/2.5af7eab3.chunk.js"
+  },
+  {
+    "revision": "271ee74172d39c0b232d",
+    "url": "/coin-flipper/static/js/main.8884bc3e.chunk.js"
+  },
+  {
+    "revision": "26f54948e695e8d62fd7",
+    "url": "/coin-flipper/static/js/runtime~main.556832e0.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/coin-flipper/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/coin-flipper/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
